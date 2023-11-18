db4d6171fe95d9e3fc247d8a948866c38ec49b25
NoobinOut <bigboi@duck.com>
2023-11-18 16:36:21 -0500

    merged changes for Milo

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -118,40 +118,7 @@ public class Robot extends TimedRobot {
       case kDefaultAuto:
       default:
         // Put default auto code here
-  public void simulationPeriodic() {
-    DifferentialDrivetrainSim m_driveSim = new DifferentialDrivetrainSim(
-  DCMotor.getNEO(2),       // 2 NEO motors on each side of the drivetrain.
-  7.29,                    // 7.29:1 gearing reduction.
-  7.5,                     // MOI of 7.5 kg m^2 (from CAD model).
-  60.0,                    // The mass of the robot is 60 kg.
-  Units.inchesToMeters(3), // The robot uses 3" radius wheels.
-  0.7112,                  // The track width is 0.7112 meters.
-
-  // The standard deviations for measurement noise:
-  // x and y:          0.001 m
-  // heading:          0.001 rad
-  // l and r velocity: 0.1   m/s
-  // l and r position: 0.005 m
-  VecBuilder.fill(0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005));
-    m_robotDrive.tankDrive(-m_controller.getLeftY(), -m_controller.getRightY());
-     // Set the inputs to the system. Note that we need to convert
-  // the [-1, 1] PWM signal to voltage by multiplying it by the
-  // robot controller voltage.
-    m_driveSim.setInputs(m_leftDrive.get() * RobotController.getInputVoltage(),
-    m_rightDrive.get() * RobotController.getInputVoltage());
-
-// Advance the model by 20 ms. Note that if you are running this
-// subsystem in a separate thread or have changed the nominal timestep
-// of TimedRobot, this value needs to match it.
-  m_driveSim.update(0.02);
-
-// Update all of our sensors.
-  m_leftEncoderSim.setDistance(m_driveSim.getLeftPositionMeters());
-  m_leftEncoderSim.setRate(m_driveSim.getLeftVelocityMetersPerSecond());
-  m_rightEncoderSim.setDistance(m_driveSim.getRightPositionMeters());
-  m_rightEncoderSim.setRate(m_driveSim.getRightVelocityMetersPerSecond());
-  //m_gyroSim.setAngle(-m_driveSim.getHeading().getDegrees());
-  }
+        break;
     }
     
   }
