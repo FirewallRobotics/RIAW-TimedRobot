5c77a31eb324106b72928f22695b462bb7c36ebb
NoobinOut <bigboi@duck.com>
2023-12-02 12:11:15 -0500

    Update Visionary.py

--- src/vision/orangepi/MilosVision/Visionary.py
+++ src/vision/orangepi/MilosVision/Visionary.py
@@ -8,6 +8,7 @@ from cscore import CameraServer
 import cv2
 import numpy as np
 
+#Camera init
 CameraServer.enableLogging()
 
 width = 160
@@ -16,14 +17,20 @@ height = 120
 camera = CameraServer.startAutomaticCapture()
 camera.setResolution(width, height)
 
+#grabs a frame from the camera
 sink = CameraServer.getVideo()
 output = CameraServer.putVideo("Cam1", width, height)
 
+#Loop does the following:
+#Grab a frame from the camera
+#Find apriltages
+#output processed image
 while True:
    time, input_img = cv2.grabFrame(input_img)
 
    if time == 0: # There is an error
       continue
+   #Takes camera image and finds apriltages
    detector = apriltag.Detector()
    result = detector.detect(input_img)
    output.putFrame(result)
