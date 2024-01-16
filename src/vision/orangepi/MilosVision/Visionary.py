7823b539aef5a7a2bf340842305cffe456bb272c
rockforit18 <156262964+rockforit18@users.noreply.github.com>
2024-01-15 23:37:18 -0500

    Updated Visionary.py

--- src/vision/orangepi/MilosVision/Visionary.py
+++ src/vision/orangepi/MilosVision/Visionary.py
@@ -7,6 +7,7 @@ import apriltag
 from cscore import CameraServer
 import cv2
 import numpy as np
+from networktables import NetworkTables
 
 #When last update print
 print("Updated 2023-12-2-117")
@@ -20,8 +21,12 @@ height = 120
 camera = CameraServer.startAutomaticCapture()
 camera.setResolution(width, height)
 
+#Init NetworkTables
+NetworkTables.initialize(server="localhost") # check the NetworkTables server address
+table = NetworkTables.getTable("Dashboard") 
+
 #grabs a frame from the camera
-sink = CameraServer.getVideo()
+cvSink = CameraServer.getVideo()
 output = CameraServer.putVideo("Cam1", width, height)
 
 #Loop does the following:
@@ -36,4 +41,18 @@ while True:
    #Takes camera image and finds apriltages
    detector = apriltag.Detector()
    result = detector.detect(input_img)
+
+  #Convert AprilTag data to a format suitable for Networktables
+   tag_data = [{"id" : detection.tag_id, "pose" : detection.pose_t} for detection in result] 
+
+   #Send data to NetworkTables 
+   try:
+       table.putString("april_tag_data". json.dumps(tag_data)) 
+   except Exception as e:
+       print("Error sending data to NetworkTables") 
+
+   for detection in result: 
+      detection.draw(input_img) 
+
+   #Output the processed image
    output.putFrame(result)
